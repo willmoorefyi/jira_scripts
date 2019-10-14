f4d7b15a9c747795b993acabad68d9f96a18c458
Will Moore <william.moore@opower.com>
2019-10-14 14:29:21 -0400

    Add project parsing

--- create_update_version.groovy
+++ create_update_version.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
 import groovy.cli.picocli.CliBuilder
+import groovy.json.JsonSlurper
 import groovy.transform.Field
 import java.net.http.*
 
 @Field final String JIRA_REST_URL = 'https://ticket.opower.com/rest/api/latest'
+@Field final JsonSlurper json = new JsonSlurper()
 
 class AuthHolder {
   static def instance
@@ -37,7 +39,16 @@ def get(path) {
       .header("Authorization", AuthHolder.getInstance().AUTH_HEADER)
       .build()
 
-  client.send(request, HttpResponse.BodyHandlers.ofString())
+  HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+  if(!response.statusCode() || response.statusCode() > 299 ) {
+    throw new RuntimeException("Authentication with input username / password failed! Server returned HTTP status ${response.statusCode()}")
+  } else if (response.statusCode() > 499) {
+    throw new RuntimeException("JIRA server error ${response.statusCode()}, please use the UI to confirm the system is up and running")
+    return 1
+  }
+
+  json.parseText response.body()
 }
 
 def env = System.getenv()
@@ -60,19 +71,18 @@ if (options.h || !options.u || !options.p || !options.c) {
 }
 
 println "Validating credentials"
-AuthHolder.initialize(options.u, options.p)
+try {
+  AuthHolder.initialize(options.u, options.p)
 
-HttpResponse<String> response = get("/mypermissions")
+  get("/mypermissions")
 
+  println "Authentication success!"
 
-if(!response.statusCode() || response.statusCode() > 299 ) {
-  println "Authentication with input username / password failed! Server returned HTTP status ${response.statusCode()}"
-  return 1
-} else if (response.statusCode() > 499) {
-  println "JIRA server error ${response.statusCode()}, please use the UI to confirm the system is up and running"
-  return 1
+  def projects = get("/project").findAll { it.projectCategory?.name == options.c }
+  println "found '${options.c}' category projects: ${projects.key}"
+
 }
-
-println "Authentication success!"
-
+catch (Exception e) {
+  println "${e.message}"
+}
 return
