ad2a766ee992641c3cc2c86b65b87c8439fcc783
Will Moore <william.moore@opower.com>
2019-10-15 00:15:35 -0400

    It works!

--- create_update_version.groovy
+++ create_update_version.groovy
@@ -1,13 +1,16 @@
 #!/usr/bin/env groovy
 import java.net.http.*
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 import groovy.cli.picocli.CliBuilder
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 import groovy.transform.Field
 
 @Field final String JIRA_REST_URL = 'https://ticket.opower.com/rest/api/latest'
 @Field final JsonSlurper json = new JsonSlurper()
+@Field final DateTimeFormatter jiraDateTimeFormat = DateTimeFormatter.ofPattern("d/MMM/yy")
 
 class AuthHolder {
   static def instance
@@ -30,15 +33,22 @@ class AuthHolder {
   }
 }
 
-def get(path) {
+def http(path, method="GET", requestBody=null) {
   HttpClient client = HttpClient.newBuilder()
       .version(HttpClient.Version.HTTP_1_1)
       .build()
+
+  def args = []
+  if (requestBody) {
+    args << requestBody
+  }
 
   HttpRequest request = HttpRequest.newBuilder()
       .uri(URI.create("${JIRA_REST_URL}${path}"))
       .header("Accepts", "application/json")
       .header("Authorization", AuthHolder.getInstance().AUTH_HEADER)
+      .header("Content-Type", "application/json;charset=UTF-8")
+      ."${method}"(*args)
       .build()
 
   HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
@@ -47,7 +57,6 @@ def get(path) {
     throw new RuntimeException("Authentication with input username / password failed! Server returned HTTP status ${response.statusCode()}")
   } else if (response.statusCode() > 499) {
     throw new RuntimeException("JIRA server error ${response.statusCode()}, please use the UI to confirm the system is up and running")
-    return 1
   }
 
   json.parseText response.body()
@@ -76,16 +85,49 @@ if (opts.h || !opts.u || !opts.p || !opts.c || !opts.n || !opts.s || !opts.r ) {
   return 1
 }
 
+final String versionName = opts.n
+final String versionStart = opts.s.format(jiraDateTimeFormat)
+final String versionRelease = opts.r.format(jiraDateTimeFormat)
+
 println "Validating credentials"
 try {
   AuthHolder.initialize(opts.u, opts.p)
 
-  get("/mypermissions")
+  http '/mypermissions'
 
   println "Authentication success!"
 
-  def projects = get("/project").findAll { it.projectCategory?.name == opts.c }
-  println "found '${opts.c}' category projects: ${projects.key}"
+  def projects = http('/project').findAll { it.projectCategory?.name == opts.c }
+  println "found '${opts.c}' category projects: ${projects*.key}"
+
+  projects.each { project ->
+    def version = http("/project/${project.key}/versions").find { it.name == versionName }
+    if (version) {
+      println """
+Updating version ${versionName} on project ${project.key}
+  from start ${version.userStartDate} and release ${version.userReleaseDate}
+  to start ${versionStart} and release ${versionRelease}
+      """
+      def request = [:]
+      request.id = version.id
+      request.name = versionName
+      request.userStartDate = versionStart
+      request.userReleaseDate = versionRelease
+
+      http "/version/${version.id}", "PUT", HttpRequest.BodyPublishers.ofString("${JsonOutput.toJson(request)}")
+    }
+    else {
+      def request = [:]
+      request.name = versionName
+      request.userStartDate = versionStart
+      request.userReleaseDate = versionRelease
+      request.projectId = project.id
+      request.project = project.key
+
+      println "Creating new version ${versionName} on project ${project.key} with start ${versionStart} and release ${versionRelease}"
+      http "/version", "POST", HttpRequest.BodyPublishers.ofString("${JsonOutput.toJson(request)}")
+    }
+  }
 
 }
 catch (Exception e) {
