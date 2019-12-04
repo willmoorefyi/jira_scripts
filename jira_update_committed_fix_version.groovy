a63e1c8d2d1993e782153e5b507336cc5f3223ac
Will Moore <william.moore@opower.com>
2019-12-04 10:33:01 -0500

    Add support for multiple fixVersions in request

--- jira_update_committed_fix_version.groovy
+++ jira_update_committed_fix_version.groovy
@@ -7,42 +7,57 @@ import com.atlassian.jira.web.bean.PagerFilter
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-final String fixVersion = "19.14"
-final String planningName = "2019 Q4 PP 19.14"
-final String dateUsed = "2019-11-20"
+final String fixVersions = "19.10,19.11,19.12,19.13,19.14,20.0,20.1,20.2,20.3,20.4"
+final String planningName = "2019 Q4 PP 19.13"
+final String dateUsed = "2019-10-30"
 
-Logger log = Logger.getLogger("com.oracle.ugbu.UpdateCommittedFixVersion")
+final Logger log = Logger.getLogger("com.oracle.ugbu.UpdateCommittedFixVersion")
 log.setLevel(Level.DEBUG)
 
-log.info("Locking in version ${fixVersion} for planning ${planningName} and date ${dateUsed}")
+def fixVersionList = []
+if (fixVersions?.trim()) {
+    log.info("Splitting fix versions ${fixVersions}")
+    fixVersionList += Arrays.asList(fixVersions.split(/,/))
+}
+else {
+    // TODO get all active versions
+    log.info("fix versions is empty")
+}
+
+log.info("Locking in version ${fixVersionList.join(",")} for planning ${planningName} and date ${dateUsed}")
 
 def committedFixVersionField = ComponentAccessor.getCustomFieldManager()
         .getCustomFieldObjectsByName("Committed Fix Version").first()
 
-def jqlQuery = "category = DSM and type = feature and status not in (Closed, Backlog) " +
-        " and fixVersion WAS \"${fixVersion}\" ON \"${dateUsed}\""
+fixVersionList.each { fixVersion ->
+    def jqlQuery = "category = DSM and type = feature and status not in (Closed, Backlog) " +
+            " and fixVersion WAS \"${fixVersion}\" ON \"${dateUsed}\""
+    log.info("Executing: ${jqlQuery}")
 
-def issueManager = ComponentAccessor.issueManager
-def user = ComponentAccessor.jiraAuthenticationContext.getLoggedInUser()
-def jqlQueryParser = ComponentAccessor.getComponent(JqlQueryParser.class)
-def searchProvider = ComponentAccessor.getComponent(SearchService.class)
+    def issueManager = ComponentAccessor.issueManager
+    def user = ComponentAccessor.jiraAuthenticationContext.getLoggedInUser()
+    def jqlQueryParser = ComponentAccessor.getComponent(JqlQueryParser.class)
+    def searchProvider = ComponentAccessor.getComponent(SearchService.class)
 
-def query = jqlQueryParser.parseQuery(jqlQuery)
-def results = searchProvider.search(user, query, PagerFilter.unlimitedFilter)
-results.issues.collect { issue -> issueManager.getIssueObject(issue.id) }
-        .each{issue ->
-            log.info("ISSUE KEY : " + issue.getKey())
-            String cfv = issue.getCustomFieldValue(committedFixVersionField) as String ?: ""
-            String value = cfv.split(/\n/)
-                    .minus("")
-                    .collect { val -> val.split(/\|/) - '' }
-                    .inject([:]) { acc, val -> acc << [ (val[0].trim()): val[1].trim() ] }
-                    .leftShift([ (planningName): fixVersion ])
-                    .sort()
-                    .inject("") { acc, val -> acc + "| ${val.key} | ${val.value} |\n" }
-                    .drop(-1)
+    def query = jqlQueryParser.parseQuery(jqlQuery)
+    def results = searchProvider.search(user, query, PagerFilter.unlimitedFilter)
+    results.issues.collect { issue -> issueManager.getIssueObject(issue.id) }
+            .each { issue ->
+                println "Operating on issue ${issue.getKey()} ..."
+                log.info("ISSUE KEY : " + issue.getKey())
+                String cfv = issue.getCustomFieldValue(committedFixVersionField) as String ?: ""
+                String value = cfv.split(/\n/)
+                        .minus("")
+                        .collect { val -> val.split(/\|/) - '' }
+                        .inject([:]) { acc, val -> acc << [(val[0].trim()): val[1].trim()] }
+                        .leftShift([(planningName): fixVersion])
+                        .sort()
+                        .inject("") { acc, val -> acc + "| ${val.key} | ${val.value} |\n" }
+                        .drop(-1)
 
-            log.info("\t\tSetting value: ${value} ")
-            issue.setCustomFieldValue(committedFixVersionField, value)
-            ComponentAccessor.getIssueManager().updateIssue(user, issue, EventDispatchOption.ISSUE_UPDATED, false)
-        }
+                log.info("\t\tSetting value: ${value} ")
+                issue.setCustomFieldValue(committedFixVersionField, value)
+                ComponentAccessor.getIssueManager().updateIssue(user, issue, EventDispatchOption.ISSUE_UPDATED, false)
+            }
+}
+println "Done"
