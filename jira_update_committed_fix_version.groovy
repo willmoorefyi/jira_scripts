05f235d014313f29f1779dfcff2283393cca3b64
Will Moore <william.moore@opower.com>
2020-01-23 10:55:49 -0500

    Updated sync script to be more flexible with extracting version.

--- jira_update_committed_fix_version.groovy
+++ jira_update_committed_fix_version.groovy
@@ -4,17 +4,16 @@ import com.atlassian.jira.event.type.EventDispatchOption
 import com.atlassian.jira.jql.parser.JqlQueryParser
 import com.atlassian.jira.project.Project
 import com.atlassian.jira.project.ProjectManager
-import com.atlassian.jira.project.version.Version
 import com.atlassian.jira.project.version.VersionManager
 import com.atlassian.jira.web.bean.PagerFilter
 
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-//final String fixVersions = "19.10,19.11,19.12,19.13,19.14,20.0,20.1,20.2,20.3,20.4"
-final String fixVersions = ""
-final String planningName = "2019 Q4 PP 19.14"
-final String dateUsed = "2019-12-18"
+final String fixVersions = "19.10,19.11,19.12,19.13,19.14,20.0,20.1,20.2,20.3,20.4,20.5,20.6,20.7,20.8,20.9,20.10"
+//final String fixVersions = ""
+final String planningName = "20.0"
+final String dateUsed = "2020-01-21"
 
 final Logger log = Logger.getLogger("com.oracle.ugbu.UpdateCommittedFixVersion")
 log.setLevel(Level.DEBUG)
@@ -59,15 +58,17 @@ fixVersionList.each { fixVersion ->
                 println "Operating on issue ${issue.getKey()} ..."
                 log.info("ISSUE KEY : " + issue.getKey())
                 String cfv = issue.getCustomFieldValue(committedFixVersionField) as String ?: ""
-                String value = cfv.split(/\n/)
+                String iterations = cfv.split(/\n/)
                         .minus("")
+                        .findAll { val -> !val.contains('||') }
                         .collect { val -> val.split(/\|/) - '' }
-                        .inject([:]) { acc, val -> acc << [(val[0].trim()): val[1].trim()] }
+                        .inject([:]) { acc, val -> acc << [(((val[0].trim()) =~ /\d{2,}\.\d{1,2}/)?.getAt(0)): val[1].trim()] }
                         .leftShift([(planningName): fixVersion])
                         .sort()
-                        .inject("") { acc, val -> acc + "| ${val.key} | ${val.value} |\n" }
+                        .inject("") { acc, val -> acc + "| PP ${val.key} | ${val.value} |\n" }
                         .drop(-1)
 
+                String value = "|| PP Iteration || Fix Version ||\n${iterations}"
                 log.info("\t\tSetting value: ${value} ")
                 issue.setCustomFieldValue(committedFixVersionField, value)
                 ComponentAccessor.getIssueManager().updateIssue(user, issue, EventDispatchOption.ISSUE_UPDATED, false)
