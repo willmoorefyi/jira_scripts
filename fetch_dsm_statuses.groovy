ffc6b5349a97eb3e66f7eb5ff3bd692b8f10194c
william.moore <william.moore@opower.com>
2021-03-08 13:47:59 -0500

    bump to bootstrap 5 beta 2

--- fetch_dsm_statuses.groovy
+++ fetch_dsm_statuses.groovy
@@ -255,7 +255,7 @@ def buildHtml(File outfile, Closure bodyClosure) {
     builder.html {
       head {
         meta charset: 'utf-8'
-        link href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css", rel:"stylesheet", integrity:"sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1", crossorigin:"anonymous"
+        link href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css", rel:"stylesheet", integrity:"sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl", crossorigin:"anonymous"
         title 'DSM Status'
       }
       body(id: 'main') {
